4f16b961108759704802c680f35b49cbfc4620ee
Nguyễn Hoàng Duy <hsbdu9@gmail.com>
2023-12-12 21:05:43 +0700

    dom-change

--- JavaS/buoi8-test/test-DOM/main.js
+++ JavaS/buoi8-test/test-DOM/main.js
@@ -1,3 +1,62 @@
+const quizes = [
+    {
+        id: 1,
+        question: "1 + 1 = ?",
+        answers: [1, 2, 3, 4],
+    },
+    {
+        id: 2,
+        question: "2 + 2 = ?",
+        answers: [2, 3, 4, 5],
+    },
+    {
+        id: 3,
+        question: "3 + 3 = ?",
+        answers: [3, 4, 5, 6],
+    },
+];
+
+function createQuizHTML(quiz) {
+    const quizItem = document.createElement('div');
+    quizItem.classList.add('quiz-item');
+
+    const questionHeader = document.createElement('h3');
+    questionHeader.textContent = `Câu ${quiz.id} : ${quiz.question}`;
+
+    const quizAnswer = document.createElement('div');
+    quizAnswer.classList.add('quiz-answer');
+
+    quiz.answers.forEach((answer, index) => {
+        const answerItem = document.createElement('div');
+        answerItem.classList.add('quiz-answer-item');
+
+        const radioInput = document.createElement('input');
+        radioInput.type = 'radio';
+        radioInput.name = quiz.id;
+        radioInput.value = answer;
+
+        const label = document.createElement('label');
+        label.textContent = answer;
+
+        answerItem.appendChild(radioInput);
+        answerItem.appendChild(label);
+        quizAnswer.appendChild(answerItem);
+    });
+
+    quizItem.appendChild(questionHeader);
+    quizItem.appendChild(quizAnswer);
+
+    return quizItem;
+}
+
+function appendQuizToContainer(quiz) {
+    const quizContainer = document.querySelector('.quiz-container');
+    const quizItem = createQuizHTML(quiz);
+    quizContainer.appendChild(quizItem);
+}
+
+appendQuizToContainer(quizes[2]);
+
 document.addEventListener("DOMContentLoaded", function () {
     // Chọn nút "Random Answer" bằng ID
     var randomAnswerButton = document.getElementById("btn");
